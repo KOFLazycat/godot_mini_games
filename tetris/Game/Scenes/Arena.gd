0493f9ea7f2c7585b72e59da26c8df6e9d3eb9ba
koflazycat <koflazycat@163.com>
2026-09-07 13:27:45 +0800

    1. 对代码进行优化

--- tetris/Game/Scenes/Arena.gd
+++ tetris/Game/Scenes/Arena.gd
@@ -1,7 +1,10 @@
+## 游戏竞技场（Arena）
+## 俄罗斯方块游戏的主场景，管理游戏状态、分数和游戏结束逻辑
 class_name Arena
 extends Node2D
 
-var clearedLines: int:
+## 已消除的行数
+var clearedLines: int = 0:
 	set(value):
 		clearedLines = value
 		score.text = str(value)
@@ -20,23 +23,34 @@ func _ready() -> void:
 	activePiece.gameOvered.connect(onGameOvered)
 
 
+## 当场地消除行时触发
+## @param lines 消除的行数
 func onPlayFieldCleared(lines: int) -> void:
 	clearedLines += lines
 
 
+## 当游戏结束时触发
+## @param type 游戏结束类型
 func onGameOvered(type: TetrominoTools.GameOverType) -> void:
 	get_tree().paused = true
-	var reasonText: String
-	match type:
-		TetrominoTools.GameOverType.OVERLAPPED:
-			reasonText = "方块重叠"
-		TetrominoTools.GameOverType.OVERFLOW:
-			reasonText = "方块溢出"
-
-	reasonLabel.text = reasonText
+	reasonLabel.text = getGameOverReason(type)
 	panel.show()
 
 
+## 获取游戏结束原因文本
+## @param type 游戏结束类型
+## @return 游戏结束原因文本
+func getGameOverReason(type: TetrominoTools.GameOverType) -> String:
+	match type:
+		TetrominoTools.GameOverType.OVERLAPPED:
+			return "方块重叠"
+		TetrominoTools.GameOverType.OVERFLOW:
+			return "方块溢出"
+		_:
+			return ""
+
+
+## 重新开始游戏
 func restart() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
