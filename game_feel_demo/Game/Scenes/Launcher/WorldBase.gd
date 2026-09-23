c07fbe71b14e746d58384d353e2e2bea2ba2f463
koflazycat <koflazycat@163.com>
2026-09-23 22:45:52 +0800

    1. 增加下雨粒子和音效

--- game_feel_demo/Game/Scenes/Launcher/WorldBase.gd
+++ game_feel_demo/Game/Scenes/Launcher/WorldBase.gd
@@ -1,2 +1,22 @@
 class_name WorldBase
 extends Node2D
+
+@export var lightningSoundResource: SoundResource
+
+@onready var juiceePlayer: JuiceePlayer = $JuiceePlayer
+
+
+var lightningTimeRemain: float = randf_range(8.0, 16.0)
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	if lightningTimeRemain <= 0:
+		juiceePlayer.play()
+		if lightningSoundResource:
+			lightningSoundResource.play_managed()
+		lightningTimeRemain = randf_range(8.0, 16.0)
+	lightningTimeRemain -= delta
